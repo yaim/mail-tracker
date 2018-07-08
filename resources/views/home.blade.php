12bc0ddccbdebd390a20f3699704208cd54bf07a
Yashar Imanlou <yashari@gmail.com>
2018-07-08 18:20:31 +0430

    Add Laravel Passport

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,9 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
                 </div>
             </div>
         </div>
