59f80a8f292fa1cf111cc63609b6d56f179dbdff
Yashar Imanlou <yashari@gmail.com>
2018-07-16 17:33:06 +0430

    Schedule emails:send

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('emails:parse')->everyMinute();
+        $schedule->command('emails:send')->everyMinute();
     }
 
     /**
