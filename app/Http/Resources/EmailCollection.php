bc2b8ee44baf49fd9b1be5777a1f0bacc7e0957f
Yashar Imanlou <yashari@gmail.com>
2019-01-06 09:45:47 +0330

    Remove extra 'data' from Email ResourceCollection

--- app/Http/Resources/EmailCollection.php
+++ app/Http/Resources/EmailCollection.php
@@ -8,18 +8,16 @@ class EmailCollection extends ResourceCollection
 {
     public function toArray($request)
     {
-        return [
-            'data' =>  $this->collection->transform(function ($email) {
-                return [
-                    'id'         => $email->id,
-                    'subject'    => $email->subject,
-                    'from'       => $email->from_email_address,
-                    'to'         => $email->to_email_address,
-                    'created_at' => (string) $email->created_at,
-                    'parsed_at'  => (string) $email->parsed_at,
-                    'sent_at'    => (string) $email->sent_at,
-                ];
-            }),
-        ];
+        return $this->collection->transform(function ($email) {
+            return [
+                'id'         => $email->id,
+                'subject'    => $email->subject,
+                'from'       => $email->from_email_address,
+                'to'         => $email->to_email_address,
+                'created_at' => (string) $email->created_at,
+                'parsed_at'  => (string) $email->parsed_at,
+                'sent_at'    => (string) $email->sent_at,
+            ];
+        });
     }
 }
