f208d211f0f7861d68c8dedc37cf792e16f2736f
Yashar Imanlou <yashari@gmail.com>
2019-01-06 09:42:25 +0330

    Add Email factory

---
+++ database/factories/EmailFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Email;
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+
+$factory->define(Email::class, function (Faker $faker) {
+    return [
+        'id'                 => $faker->unique()->uuid,
+        'from_email_address' => $faker->safeEmail,
+        'to_email_address'   => $faker->safeEmail,
+        'subject'            => $faker->text,
+        'content'            => $faker->randomHTML,
+        'user_id'            => 1,
+    ];
+});
+
+$factory->state(Email::class, 'raw', [
+    'parsed_content' => null,
+    'parsed_at'      => null,
+]);
+
+$factory->state(Email::class, 'parsed', function (Faker $faker) {
+    $content = $faker->randomHTML;
+    $id = $faker->unique()->uuid;
+
+    return [
+        'id'             => $id,
+        'content'        => $content,
+        'parsed_content' => '<img src="'.route('tracking.email', $id).'">'.$content,
+        'parsed_at'      => Carbon::parse('-1 hours'),
+    ];
+});
