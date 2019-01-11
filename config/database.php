1e926b003c018923e94491badce3b911605bc46a
Yashar Imanlou <yashari@gmail.com>
2019-01-11 19:09:40 +0330

    Add travis-ci

--- config/database.php
+++ config/database.php
@@ -78,6 +78,17 @@ return [
             'prefix' => '',
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'database'  => env('DB_DATABASE', 'mail_tracker'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
     ],
 
     /*
