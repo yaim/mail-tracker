07967ac95234727ba6fd18a47f8ba07e14012c26
Yashar Imanlou <yashari@gmail.com>
2019-01-15 03:25:15 +0330

    Create Feature/ViewReportsTest

---
+++ tests/Feature/ViewReportsTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Email;
+use App\Http\Resources\Email as EmailResource;
+use App\Http\Resources\EmailCollection as EmailCollectionResource;
+use App\Repositories\Contracts\EmailRepositoryInterface as EmailRepository;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewReportsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->emailRepository = resolve(EmailRepository::class);
+    }
+
+    public function testUserCanGetOwnedPostedEmailData()
+    {
+        $user = factory(User::class)->create();
+        $emailId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+
+        $email = (new EmailResource(factory(Email::class)->states('parsed')->create([
+            'id'                 => $emailId,
+            'from_email_address' => 'j.cash@example.com',
+            'to_email_address'   => 'j.carter.cash@example.com',
+            'subject'            => 'Happy Birthday Princess',
+            'content'            => 'We get old and get use to each other. We think alike.',
+            'parsed_content'     => '<img src="'.route('tracking.email', $emailId).'">We get old and get use to each other. We think alike.',
+            'parsed_at'          => Carbon::parse('23 June 1994'),
+            'user_id'            => $user->id,
+        ])))->toArray(null);
+
+        $response = $this->actingAs($user, 'api')->get(route('email.show', $emailId));
+
+        $response->assertStatus(200)
+                 ->assertExactJson([
+                    'data' => $email,
+                ]);
+    }
+
+    public function testUserCanListOwnedPostedEmails()
+    {
+        $user = factory(User::class)->create(['id' => 100]);
+        $emails = (new EmailCollectionResource(collect([
+            factory(Email::class)->states('parsed')->create(['user_id' => 100]),
+            factory(Email::class)->states('parsed')->create(['user_id' => 100]),
+            factory(Email::class)->states('parsed')->create(['user_id' => 100]),
+            factory(Email::class)->states('parsed')->create(['user_id' => 100]),
+            factory(Email::class)->states('parsed')->create(['user_id' => 100]),
+        ])))->toArray(null);
+
+        $response = $this->actingAs($user, 'api')->get(route('email.index'));
+
+        $response->assertStatus(200)
+                 ->assertExactJson(['data' => $emails->toArray()]);
+    }
+
+    public function testUnauthenticatedUserCannotSeePostedEmails()
+    {
+        $emailId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+
+        factory(Email::class)->states('parsed')->create([
+            'id' => $emailId,
+        ]);
+        factory(Email::class, 4)->states('parsed')->create();
+
+        $emailsList = $this->get(route('email.index'));
+        $singleEmail = $this->get(route('email.show', $emailId));
+
+        $this->assertEquals($this->emailRepository->count(), 5);
+        $emailsList->assertRedirect(route('login'));
+        $singleEmail->assertRedirect(route('login'));
+    }
+
+    public function testUserCannotSeeOthersPostedEmails()
+    {
+        $june = factory(User::class)->create(['id' => 100]);
+        $johnny = factory(User::class)->create(['id' => 101]);
+        $emailId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+
+        factory(Email::class)->states('parsed')->create([
+            'id'      => $emailId,
+            'user_id' => $june->id,
+        ]);
+        factory(Email::class, 4)->states('parsed')->create([
+            'user_id' => $june->id,
+        ]);
+
+        $emailsList = $this->actingAs($johnny, 'api')->get(route('email.index'));
+        $singleEmail = $this->actingAs($johnny, 'api')->get(route('email.show', $emailId));
+
+        $this->assertEquals($this->emailRepository->count(), 5);
+        $emailsList->assertStatus(200)
+                   ->assertExactJson(['data' => []]);
+        $singleEmail->assertStatus(404);
+    }
+}
